a1eaf8719b7e34d16a0fa237a650d0a7ed2a62f3
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2022-08-31 21:02:55 -0300

    table

--- public/js/user.js
+++ public/js/user.js
@@ -12,16 +12,49 @@ form.addEventListener("submit", () => {
 
       } else {
 
-        document.getElementById("vini").innerHTML += "<table border=1 width=100%><tr>";
-        for(const interator of data.success)
-        {
-            document.getElementById("vini").innerHTML += "<td align=center style='color:#fff'>"+interator.email+"</td>";
+        const interator = data.success;
+          // Extract value from table header. 
+          // ('Book ID', 'Book Name', 'Category' and 'Price')
+          let col = [];
+          for (let i = 0; i < interator.length; i++) {
+            for (let key in interator[i]) {
+              if (col.indexOf(key) === -1) {
+                col.push(key);
+              }
+            }
+          }
+      
+          // Create table.
+          const table = document.createElement("table");
+      
+          // Create table header row using the extracted headers above.
+          let tr = table.insertRow(-1);                   // table row.
+      
+          for (let i = 0; i < col.length; i++) {
+            let th = document.createElement("th");      // table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+          }
+      
+          // add json data to the table as rows.
+          for (let i = 0; i < interator.length; i++) {
+      
+            tr = table.insertRow(-1);
+      
+            for (let j = 0; j < col.length; j++) {
+              let tabCell = tr.insertCell(-1);
+              tabCell.innerHTML = interator[i][col[j]];
+            }
+          }
+      
+          // Now, add the newly created table with json data, to a container.
+          const divShowData = document.getElementById('vini');
+          divShowData.innerHTML = "";
+          divShowData.appendChild(table);
         }
-        document.getElementById("vini").innerHTML += "</tr></table>";
-
-        console.log(data.success)
         return data.success
         console.log("sucesso!")
       }
-    })
+    
+    )
 })
